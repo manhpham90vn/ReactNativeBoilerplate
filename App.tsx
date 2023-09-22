5720da24873c97a27b3c74fdd7675a48d8c89c93
Manh Pham <manhpham90vn@icloud.com>
2023-09-22 17:48:09 +0700

    add login screen

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import { Provider } from 'react-redux';
 import RootNavigationStack from './src/navigations/rootNavigationStack';
 import { store } from './src/redux/store';
 import Example1 from './src/screens/example1';
+import Example2 from './src/screens/example2';
 
 const App = () => {
-  return <Example1 />;
+  return <Example2 />;
   return (
     <Provider store={store}>
       <RootNavigationStack />
